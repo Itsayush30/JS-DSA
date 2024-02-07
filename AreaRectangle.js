03163cf921b3137b5623eb14796f6f68c5a17e7c
itsayush30 <itsayush30@gmail.com>
2024-02-07 21:07:39 +0530

    area of rectangle

---
+++ AreaRectangle.js
@@ -0,0 +1,17 @@
+// Function to Find the Area of Triangle
+function areaRectangle(a, b) {
+  let area = a * b;
+  return area;
+}
+
+// Function to Find the Parameter of Triangle
+function perimeterRectangle(a, b) {
+  let perimeter = 2 * (a + b);
+  return perimeter;
+}
+
+// Driver program
+let a = 5;
+let b = 6;
+console.log("Area = " + areaRectangle(a, b));
+console.log("Perimeter = " + perimeterRectangle(a, b));
