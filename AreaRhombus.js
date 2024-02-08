a3a64f8f84292d6ca5953d4b8ca611d93dd20a01
itsayush30 <itsayush30@gmail.com>
2024-02-08 22:52:01 +0530

    area of rhombus

---
+++ AreaRhombus.js
@@ -0,0 +1,9 @@
+function rhombusArea(diagonal1, diagonal2) {
+    return (diagonal1 * diagonal2) / 2;
+}
+
+// Example usage:
+let diagonal1 = 8;
+let diagonal2 = 6;
+let area = rhombusArea(diagonal1, diagonal2);
+console.log("Area of the rhombus:", area);
