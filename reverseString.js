61db46be8f99fc63f851a874c143672cd29e0726
itsayush30 <itsayush30@gmail.com>
2024-02-09 22:49:18 +0530

    reverse string

---
+++ reverseString.js
@@ -0,0 +1,8 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Example usage:
+const originalString = "dsa";
+const reversedString = reverseString(originalString);
+console.log(reversedString); // Output: asd
